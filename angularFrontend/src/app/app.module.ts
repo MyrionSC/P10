22be0565d344ac5838c8d21a11d91865e27fa7d2
Rasmus Møller Jensen <jensen.rasmusmoller@gmail.com>
2019-02-14 14:21:24 +0100

    Reformat

--- angularFrontend/src/app/app.module.ts
+++ angularFrontend/src/app/app.module.ts
@@ -1,23 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-
-import { LeafletModule } from '@asymmetrik/angular2-leaflet';
-
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import {LeafletModule} from '@asymmetrik/angular2-leaflet';
+import {AppComponent} from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-      FormsModule,
-    HttpClientModule,
-    LeafletModule.forRoot()
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpClientModule,
+        LeafletModule.forRoot()
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
