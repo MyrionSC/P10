0b1a150ade95100fe4f7dfcf53e98eb7b6419b61
Rasmus Møller Jensen <jensen.rasmusmoller@gmail.com>
2019-02-14 08:58:01 +0100

    Begin implementation of a GUI for the API

--- API/main.py
+++ API/main.py
@@ -1,6 +1,24 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
+import psycopg2
 import json
 app = Flask(__name__)
+
+database = {
+    "name": "ev_smartmi",
+    "user": "smartmi",
+    "port": "4102",
+    "host": "172.19.1.104",
+    "password": "63p467R=530"
+}
+
+def query(str):
+    conn = psycopg2.connect("dbname='{0}' user='{1}' port='{2}' host='{3}' password='{4}'".format(database['name'], database['user'], database['port'], database['host'], database['password']))
+    cur = conn.cursor()
+    cur.execute(str)
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+    return rows
 
 @app.route("/")
 def hello():
@@ -8,25 +26,37 @@ def hello():
 
 @app.route("/route")
 def route():
-    origin = request.args.get('origin')
-    dest = request.args.get('dest')
-    dic = {
-	'origin': origin,
-	'dest': dest,
-	'energy': 42
-    }
-    return json.dumps(dic)
+    origin = int(request.args.get('origin'))
+    dest = int(request.args.get('dest'))
+    geojson = get_route(origin, dest)
+    return geojson
+    
 
-
-
-
-#@app.route("/test/<int:num>")
-#def test(num):
-    #return str(testfunc(num,2))
-
-
-
+def get_route(origin, dest):
+    qry = """
+    SELECT row_to_json(fc)
+        FROM(
+            SELECT
+                'FeaturesCollection' as "type",
+                array_to_json(array_agg(f)) as "features"
+            FROM (
+                SELECT
+                    'Feature' as "type",
+                    ST_AsGeoJSON(segmentgeo, 6) :: json as "geometry"
+                FROM (
+                    SELECT
+                        osm_dk_20140101.segmentgeo
+                    FROM pgr_dijkstra('SELECT edg.*, meters as cost FROM experiments.rmp10_edges edg JOIN maps.osm_dk_20140101 osm ON osm.segmentkey = edg.segmentkey'::text, {0}::bigint, {1}::bigint) pgr
+                    JOIN maps.osm_dk_20140101 ON pgr.edge = osm_dk_20140101.segmentkey
+                ) as q
+            ) as f
+        ) as fc
+    """.format(origin, dest)
+    return query(qry)[0][0]
 
 def testfunc(x, y):
     return x ** y 
 
+@app.route("/test_route")
+def test():
+    return render_template('test.html')
