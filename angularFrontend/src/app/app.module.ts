91a13c1038ad474ef356e144046c590527bed565
Rasmus Møller Jensen <jensen.rasmusmoller@gmail.com>
2019-02-14 13:44:47 +0100

    Implement new angular project

---
+++ angularFrontend/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { LeafletModule } from '@asymmetrik/angular2-leaflet';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    LeafletModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
