03316b69d939906590c62fb9e484725311b1b3ba
myrionsc <marpht23@gmail.com>
2019-02-14 13:18:05 +0100

    leaflet stuff and conf

---
+++ angcli/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    LeafletModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
