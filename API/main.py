0dd9c5553a094716807e61884b4211313a531dd2
Rasmus Møller Jensen <jensen.rasmusmoller@gmail.com>
2019-02-15 14:30:20 +0100

    Update backend to use local database

--- API/main.py
+++ API/main.py
@@ -2,25 +2,10 @@ from flask import Flask, request, render_template, send_from_directory, redirect
 from flask_cors import CORS
 import psycopg2
 import json
+from model import *
+
 app = Flask(__name__, static_folder="map")
 CORS(app)
-
-database = {
-    "name": "ev_smartmi",
-    "user": "smartmi",
-    "port": "4102",
-    "host": "172.19.1.104",
-    "password": "63p467R=530"
-}
-
-def query(str):
-    conn = psycopg2.connect("dbname='{0}' user='{1}' port='{2}' host='{3}' password='{4}'".format(database['name'], database['user'], database['port'], database['host'], database['password']))
-    cur = conn.cursor()
-    cur.execute(str)
-    rows = cur.fetchall()
-    cur.close()
-    conn.close()
-    return rows
 
 @app.route("/map") # serve frontend, which is in map dir
 def map():
@@ -32,26 +17,7 @@ def get_json():
     dest = int(request.args.get('dest'))
     return get_route(origin, dest)
 
-
-def get_route(origin, dest):
-    qry = """
-    SELECT row_to_json(fc)::text as path
-        FROM(
-            SELECT
-                'FeaturesCollection' as "type",
-                array_to_json(array_agg(f)) as "features"
-            FROM (
-                SELECT
-                    'Feature' as "type",
-                    ST_AsGeoJSON(the_geom, 6) :: json as "geometry"
-                FROM (
-                    SELECT
-                        the_geom
-                    FROM pgr_dijkstra('SELECT edg.*, meters as cost FROM experiments.rmp10_edges edg JOIN maps.osm_dk_20140101 osm ON osm.segmentkey = edg.segmentkey'::text, {0}::bigint, {1}::bigint) pgr
-                    JOIN experiments.rmp10_edges osm
-                    ON pgr.edge = osm.id
-                ) as q
-            ) as f
-        ) as fc
-    """.format(origin, dest)
-    return query(qry)[0][0]
+@app.route("/embedding")
+def emb():
+    key = int(request.args.get('key'))
+    return get_embedding(key)
---
+++ API/model.py
@@ -0,0 +1,61 @@
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import pandas as pd
+
+dijkstra_qry = '\'SELECT segmentkey as id, startpoint as source, endpoint as target, segmentgeom as the_geom, length as cost FROM maps.routing\''
+
+main_db = {
+    "name": "ev_smartmi",
+    "user": "smartmi",
+    "port": "4102",
+    "host": "172.19.1.104",
+    "password": "63p467R=530"
+}
+
+local_db = {
+    "name": "postgres",
+    "user": "postgres",
+    "port": "5432",
+    "host": "localhost",
+    "password": "Agerbjerg93"
+}
+
+def query(str, db):
+    conn = psycopg2.connect("dbname='{0}' user='{1}' port='{2}' host='{3}' password='{4}'".format(db['name'], db['user'], db['port'], db['host'], db['password']))
+    cur = conn.cursor()
+    cur.execute(str)
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+    return rows
+
+def get_route(origin, dest):
+    qry = """
+    	SELECT row_to_json(fc)::text as path
+        FROM(
+            SELECT
+                'FeaturesCollection' as "type",
+                array_to_json(array_agg(f)) as "features"
+            FROM (
+                SELECT
+                    'Feature' as "type",
+                    ST_AsGeoJSON(segmentgeom, 6) :: json as "geometry"
+                FROM (
+                    SELECT
+                        segmentgeom
+                    FROM pgr_dijkstra({2}::text, {0}::bigint, {1}::bigint) pgr
+                    JOIN maps.routing osm
+                    ON pgr.edge = osm.segmentkey
+                ) as q
+            ) as f
+        ) as fc
+    """.format(origin, dest, dijkstra_qry)
+    return query(qry, local_db)[0][0]
+
+def get_embedding(key):
+	qry = """
+		SELECT *
+		FROM embeddings.line
+		WHERE segmentkey = {0}
+	""".format(key)
+	return str(list(query(qry, local_db)[0])[1:])
