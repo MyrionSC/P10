ad4c54f040e58f52c4608a776bcd5086469a0c09
Rasmus Møller Jensen <jensen.rasmusmoller@gmail.com>
2019-02-15 15:19:37 +0100

    Remember cost sql queries

--- SQL/pg_routing_setup.sql
+++ SQL/pg_routing_setup.sql
@@ -3,3 +3,11 @@ SELECT segmentkey, startpoint, endpoint, segmentgeo::geometry as segmentgeom
 FROM maps.osm_dk_20140101;
 
 SELECT pgr_createTopology('maps.routing', 0.001, 'segmentgeom', 'segmentkey', 'startpoint', 'endpoint');
+
+ALTER TABLE maps.routing
+ADD COLUMN length float;
+
+UPDATE maps.routing
+SET length = maps.osm_dk_20140101.meters
+FROM maps.osm_dk_20140101
+WHERE segmentkey = maps.osm_dk_20140101.segmentkey;
