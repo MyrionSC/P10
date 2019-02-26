4b4ae6fa678a1376da6323136627e070f9fc6baa
Rasmus Møller Jensen <jensen.rasmusmoller@gmail.com>
2019-02-26 10:40:26 +0100

    Implement batch training script

--- Models/Keras-Regressor/DNN-BatchTrain.py
+++ Models/Keras-Regressor/DNN-BatchTrain.py
@@ -2,9 +2,22 @@ import DNN_Regressor as DNN
 import Configuration
 
 
-default_config = Configuration.energy_config
-configs = [default_config]
-configs[0]['epochs'] = 1
+def generate_configs():
+    batch_name = "TestBatch1"
+    default_config = Configuration.energy_config
+    default_config['batch_dir'] = batch_name + "/"
+
+    confs = [{'epochs': 10}]
+    # Generate configurations here and add them to the confs list
+    new_confs = []
+    for i in range(len(confs)):
+        new_confs.append(default_config.copy())
+        new_confs[i].update(confs[i])
+
+    return new_confs
+
+
+configs = generate_configs()
 
 for config in configs:
     DNN.train(config)
