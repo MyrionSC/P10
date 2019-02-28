a7894029b942fd59732221fb13165c37d16297a0
Rasmus Møller Jensen <jensen.rasmusmoller@gmail.com>
2019-02-28 15:41:43 +0100

    Add regularization

--- Models/Keras-Regressor/Model.py
+++ Models/Keras-Regressor/Model.py
@@ -2,6 +2,7 @@ from keras import Sequential, initializers
 from keras.layers import Dense, Dropout
 from keras.utils import multi_gpu_model
 from Metrics import rmse, r2
+from keras.regularizers import l2
 
 
 # Model definition for a simple linear regressor
@@ -20,13 +21,13 @@ def DNNRegressor(input_dim, output_dim, hidden_layers, cells_per_layer, activati
     model = Sequential()
 
     # Adds input layer and first hidden layer
-    model.add(Dense(cells_per_layer, input_dim=input_dim, kernel_initializer=kernel_initializer, bias_initializer=initializers.Constant(0.1), activation=activation))
+    model.add(Dense(cells_per_layer, input_dim=input_dim, kernel_initializer=kernel_initializer, bias_initializer=initializers.Constant(0.1), activation=activation, activity_regularizer=l2(0.001)))
     if initial_dropout > 0:
         model.add(Dropout(initial_dropout))
 
     # Adds k-1 hidden layers
     for i in range(hidden_layers - 1):
-        model.add(Dense(cells_per_layer, kernel_initializer=kernel_initializer, bias_initializer=initializers.Constant(0.1), activation=activation))
+        model.add(Dense(cells_per_layer, kernel_initializer=kernel_initializer, bias_initializer=initializers.Constant(0.1), activation=activation, activity_regularizer=l2(0.001)))
         if dropout > 0:
             model.add(Dropout(dropout))
 
