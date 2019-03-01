918d97b429350fc83080c2bb0f57aa449e0ef120
Rasmus Møller Jensen <jensen.rasmusmoller@gmail.com>
2019-03-01 13:38:04 +0100

    Include intersection as features

--- Models/Keras-Regressor/DNNBatchTrain.py
+++ Models/Keras-Regressor/DNNBatchTrain.py
@@ -10,7 +10,7 @@ def generate_configs() -> List[Config]:
     default_config = Configuration.energy_config
     default_config['batch_dir'] = batch_name + "/"
 
-    confs = [{'features_used': ['incline', 'segment_length', 'categoryid', 'speedlimit']}]
+    confs = [{'features_used': ['incline', 'segment_length', 'categoryid', 'speedlimit', 'intersection']}]
     # Generate configurations here and add them to the confs list
     new_confs = []
     for i in range(len(confs)):
