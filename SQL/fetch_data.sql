25fa0387172fecc10e16bae255ab5263ffb0ebdb
Rasmus Møller Jensen <jensen.rasmusmoller@gmail.com>
2019-03-01 13:05:36 +0100

    Update fetch script to work again

--- SQL/fetch_data.sql
+++ SQL/fetch_data.sql
@@ -1,37 +1,38 @@
 DROP VIEW IF EXISTS experiments.segments_temp_view;
 CREATE VIEW experiments.segments_temp_view AS
-        trips_table.id as mapmatched_id,
-        trips_table.segmentkey as segmentkey,
-        osm_map.categoryid,
-        CASE WHEN incline_table.incline_percentage IS NOT NULL
-             THEN CASE WHEN trips_table.direction = 'BACKWARD'
-                       THEN -incline_table.incline_percentage
-                       ELSE incline_table.incline_percentage
-                  END 
-             ELSE 0
-        END AS incline,
-        trips_table.meters_segment as segment_length,
-        trips_table.speed / 3.6 as speed,
-        weather_table.air_temperature as temperature,
-        CASE WHEN wind_table.tailwind_magnitude IS NOT NULL
-             THEN -wind_table.tailwind_magnitude 
-             ELSE 0
-        END as headwind_speed,
-        time_table.quarter,
-        date_table.weekday,
-        date_table.month,
-        trips_table.ev_kwh,
-        speedlimit_table.speedlimit,
-        inter_table.intersection
+SELECT
+    trips_table.id as mapmatched_id,
+    trips_table.segmentkey as segmentkey,
+    osm_map.categoryid,
+    CASE WHEN incline_table.incline_percentage IS NOT NULL
+         THEN CASE WHEN trips_table.direction = 'BACKWARD'
+                   THEN -incline_table.incline_percentage
+                   ELSE incline_table.incline_percentage
+              END 
+         ELSE 0
+    END AS incline,
+    trips_table.meters_segment as segment_length,
+    trips_table.speed / 3.6 as speed,
+    weather_table.air_temperature as temperature,
+    CASE WHEN wind_table.tailwind_magnitude IS NOT NULL
+         THEN -wind_table.tailwind_magnitude 
+         ELSE 0
+    END as headwind_speed,
+    time_table.quarter,
+    date_table.weekday,
+    date_table.month,
+    trips_table.ev_kwh,
+    speedlimit_table.speedlimit,
+    inter_table.intersection
 FROM experiments.mi904e18_training as trips_table, 
-        maps.osm_dk_20140101 as osm_map,
-        dims.dimdate as date_table, 
-        dims.dimtime as time_table, 
-        experiments.mi904e18_segment_incline as incline_table,
-        dims.dimweathermeasure as weather_table, 
-        experiments.mi904e18_wind_vectors as wind_table,
-        experiments.mi904e18_speedlimits as speedlimit_table,
-        experiments.rmp10_intersections as inter_table
+    maps.osm_dk_20140101 as osm_map,
+    dims.dimdate as date_table, 
+    dims.dimtime as time_table, 
+    experiments.mi904e18_segment_incline as incline_table,
+    dims.dimweathermeasure as weather_table, 
+    experiments.mi904e18_wind_vectors as wind_table,
+    experiments.mi904e18_speedlimits as speedlimit_table,
+    experiments.rmp10_intersections as inter_table
 WHERE trips_table.segmentkey = osm_map.segmentkey 
     AND trips_table.datekey = date_table.datekey 
     AND trips_table.timekey = time_table.timekey 
