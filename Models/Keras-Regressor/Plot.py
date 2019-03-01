e48c560c4ebb0ce0c2db417a704ef810b5c978db
myrionsc <marpht23@gmail.com>
2019-03-01 12:13:47 +0100

    help for plot.py

--- Models/Keras-Regressor/Plot.py
+++ Models/Keras-Regressor/Plot.py
@@ -1,6 +1,13 @@
 from Plots import plot_history, load_hist
 import sys
 
+help = "Usage: Argument 1 to n should be paths to models. The plots end up in MODEL_DIR/plots."
+if len(sys.argv) < 2:
+    print(help)
+    exit()
+
+
+
 args = sys.argv[1:]
 model_path = args[0]
 config, history = load_hist(model_path)
