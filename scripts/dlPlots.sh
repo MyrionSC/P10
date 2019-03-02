30c50bcabdb058b447869c91ff100c6a1f30487a
myrionsc <marpht23@gmail.com>
2019-03-02 17:50:00 +0100

    added 'latest' argument for dlPlots, to download plots from latest trained batch of models

--- scripts/dlPlots.sh
+++ scripts/dlPlots.sh
@@ -2,27 +2,34 @@
 
 
 if [ "$#" -ne 1 ]; then
-    echo "Usage: First argument should be a model batch dir in /home/rmp10/P10/Models/Keras-Regressor/saved_models on the server."
+    echo "Usage: First argument should be a model batch dir in /home/rmp10/P10/Models/Keras-Regressor/saved_models on the server, or 'latest' to get the most recently trained batch"
+    exit
 fi
 
-
 P10=rmp10@172.25.11.191
-ModelBatchPath="/home/rmp10/P10/Models/Keras-Regressor/saved_models/$1"
 
+
+if [ "$1" = "latest" ]; then
+    BatchName=$(ssh $P10 'ls /home/rmp10/P10/Models/Keras-Regressor/saved_models -t | head -1')
+else
+    BatchName="$1"
+fi
+
+ModelBatchPath="/home/rmp10/P10/Models/Keras-Regressor/saved_models/$BatchName"
 Modeldirs=$(ssh $P10 "ls $ModelBatchPath")
 
 if [ "$Modeldirs" = "" ]; then
     exit
 fi
 
-rm -rf "$1-plots"
-mkdir "$1-plots"
+rm -rf "$BatchName-plots"
+mkdir "$BatchName-plots"
 
 for model in $Modeldirs; do
     if [ "$model" = "runtime.txt" ]; then
         continue
     fi
-    modelDir="$1-plots/$model-plots"
+    modelDir="$BatchName-plots/$model-plots"
     rm -rf $modelDir
     mkdir $modelDir
     scp $P10:$ModelBatchPath/$model/plots/* $modelDir
