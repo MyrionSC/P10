1daa094d5715d2318026c7a44f49ff5e57600302
Rasmus Møller Jensen <jensen.rasmusmoller@gmail.com>
2019-03-04 14:34:55 +0100

    Add MAE and MSE batch configurations

---
+++ Models/Keras-Regressor/batch_configs/mae.py
@@ -0,0 +1,22 @@
+import Configuration
+
+batch_name = "MeanAbsoluteError"
+default_config = Configuration.energy_config
+default_config['batch_dir'] = batch_name + "/"
+configs = []
+
+configMAE = default_config.copy()
+configMAE['model_name_base'] = "MAE"
+configMAE['embedding'] = 'node2vec-64d'
+configMAE['features_used'] = ['categoryid', 'segment_length', 'height_change', 'temperature', 'quarter', 'weekday', 'month']
+configMAE['loss'] = 'mae'
+configMAE['scale'] = False
+configs.append(configMAE)
+
+configMAEScale = default_config.copy()
+configMAEScale['model_name_base'] = "MAE_Scale"
+configMAEScale['embedding'] = 'node2vec-64d'
+configMAEScale['features_used'] = ['categoryid', 'segment_length', 'height_change', 'temperature', 'quarter', 'weekday', 'month']
+configMAEScale['loss'] = 'mae'
+configMAEScale['scale'] = True
+configs.append(configMAEScale)
