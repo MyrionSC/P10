99db7742fdd6c61fe7ea2e42a6eba702c91fd4af
myrionsc <marpht23@gmail.com>
2019-03-04 14:46:11 +0100

    some refactor in dlPlots

--- scripts/dlPlots.sh
+++ scripts/dlPlots.sh
@@ -10,28 +10,30 @@ P10=rmp10@172.25.11.191
 
 
 if [ "$1" = "latest" ]; then
-    BatchName=$(ssh $P10 'ls /home/rmp10/P10/Models/Keras-Regressor/saved_models -t | head -1')
+    BATCHNAME=$(ssh $P10 'ls /home/rmp10/P10/Models/Keras-Regressor/saved_models -t | head -1')
 else
-    BatchName="$1"
+    BATCHNAME="$1"
 fi
 
-ModelBatchPath="/home/rmp10/P10/Models/Keras-Regressor/saved_models/$BatchName"
-Modeldirs=$(ssh $P10 "ls $ModelBatchPath")
+echo "Getting plots for batch: $BATCHNAME"
+MODELBATCHPATH="/home/rmp10/P10/Models/Keras-Regressor/saved_models/$BATCHNAME"
+MODELDIRS=$(ssh $P10 "ls $MODELBATCHPATH")
 
-if [ "$Modeldirs" = "" ]; then
+if [ "$MODELDIRS" = "" ]; then
     exit
 fi
 
-rm -rf "$BatchName-plots"
-mkdir "$BatchName-plots"
+rm -rf "$BATCHNAME-plots"
+mkdir "$BATCHNAME-plots"
 
-for model in $Modeldirs; do
-    if [ "$model" = "runtime.txt" ]; then
+for MODEL in $MODELDIRS; do
+    if [ "$MODEL" = "runtime.txt" ]; then
         continue
     fi
-    modelDir="$BatchName-plots/$model-plots"
-    rm -rf $modelDir
-    mkdir $modelDir
-    scp $P10:$ModelBatchPath/$model/plots/* $modelDir
+    MODELDIR="$BATCHNAME-plots/$MODEL-plots"
+    echo "copying plots for $MODEL into $MODELDIR"
+    rm -rf $MODELDIR
+    mkdir $MODELDIR
+    scp $P10:$MODELBATCHPATH/$MODEL/plots/* $MODELDIR
 done
 
