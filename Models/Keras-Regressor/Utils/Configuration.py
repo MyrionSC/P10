b1557f1af7939bbd5e1f3512903a54d447b81244
Rasmus Møller Jensen <jensen.rasmusmoller@gmail.com>
2019-03-06 11:06:43 +0100

    Auto stash before merge of "master" and "origin/master"

--- Models/Keras-Regressor/Utils/Configuration.py
+++ Models/Keras-Regressor/Utils/Configuration.py
@@ -82,8 +82,8 @@ energy_config.update({
     'batch_dir': "Default_Energy_Models/",
     'scale': True,
     'cyclic_quarter': False,
-    'loss': 'mse',
-    'speed_model_path': 'saved_models/Default_Speed_Models/MSE_TanHepochs_10-hidden_layers_6-cells_per_layer_1000-embeddings_node2vec-64d' # should be relative path from Keras, eg: saved_models/Speed_Models/Some_Model
+    'loss': 'mae',
+    'speed_model_path': 'saved_models/SpeedActivationLossGrid/MSE_TanHepochs_10-hidden_layers_6-cells_per_layer_1000-embeddings_node2vec-64d' # should be relative path from Keras, eg: saved_models/Speed_Models/Some_Model
 })
 
 # Possible features: ['categoryid', 'incline', 'segment_length', 'speed_prediction', 'height_change', 'speed', 'temperature', 'headwind_speed', 'quarter', 'weekday', 'month', 'speedlimit', 'intersection']
---
+++ Models/Keras-Regressor/batch_configs/speed_dropout.py
@@ -0,0 +1,20 @@
+from Utils import Configuration
+
+batch_name = "SpeedDropout"
+default_config = Configuration.speed_config
+default_config['batch_dir'] = batch_name + "/"
+configs = []
+
+actii = ['relu', 'softsign', 'tanh']
+init_dropout = [0.0, 0.25, 0.5]
+dropout = [0.0, 0.25, 0.5]
+
+for initial in init_dropout:
+    for rest in dropout:
+        for act in actii:
+            config = default_config.copy()
+            config['model_name_base'] = "Speed-" + act.upper() + "-Init_{0.1f}-Dropout_{1.1f}".format(initial, rest)
+            config['initial_dropout'] = initial
+            config['dropout'] = rest
+            config['activation'] = act
+            configs.append(config)
