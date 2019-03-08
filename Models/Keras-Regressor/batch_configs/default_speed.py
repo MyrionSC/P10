fe7a06d3cd82f1c2393a4a17ff9b68d03ccfb604
Rasmus Møller Jensen <jensen.rasmusmoller@gmail.com>
2019-03-08 11:06:53 +0100

    Add default speed predictor trained for 20 epochs

---
+++ Models/Keras-Regressor/batch_configs/default_speed.py
@@ -0,0 +1,12 @@
+from Utils import Configuration
+
+batch_name = "Default_Speed_Models"
+default_config = Configuration.speed_config
+default_config['batch_dir'] = batch_name + "/"
+configs = []
+
+config = default_config.copy()
+config['epocs'] = 20
+config['model_name_base'] = "DefaultSpeed-epochs_20"
+configs.append(config)
+
