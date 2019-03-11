e8580fe136a43507d4f1ab55608c636b86cf8bae
Rasmus Møller Jensen <jensen.rasmusmoller@gmail.com>
2019-03-11 10:03:03 +0100

    Ensure that config is copied properly

--- Models/Keras-Regressor/batch_configs/time_interval.py
+++ Models/Keras-Regressor/batch_configs/time_interval.py
@@ -13,15 +13,13 @@ for x in intervals:
     config = speed_config.copy()
     speed_name = "Speed-" + x.upper()
     config['model_name_base'] = speed_name
-    if 'quarter' in config['features_used']:
-        config['features_used'].remove('quarter')
-    config['features_used'].append(x)
+    config['features_used'] = [y for y in config['features_used'] if y != "quarter"] + [x]
     configs.append(config)
 
     config = energy_config.copy()
     config['model_name_base'] = "Energy-" + x.upper()
-    if 'quarter' in config['features_used']:
-        config['features_used'].remove('quarter')
-    config['features_used'].append(x)
+    config['features_used'] = [y for y in config['features_used'] if y != "quarter"] + [x]
     config['speed_model_path'] = "saved_models/" + batch_name + "/" + speed_name
     configs.append(config)
+
+print("end")
