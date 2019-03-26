3d753d8aafd4f11c8e0f548f73d3e1bf5f98ab9f
myrionsc <marpht23@gmail.com>
2019-03-26 10:34:51 +0100

    updated supersegs motorway sql

--- SQL/supersegments_motorways_links.sql
+++ SQL/supersegments_motorways_links.sql
@@ -1,26 +1,30 @@
 drop table if exists experiments.rmp10_supersegments_motorway_links;
 create table experiments.rmp10_supersegments_motorway_links as
 (select 
+ 	links.segmentkey as link_segment,
 	array[links.segmentkey, motors.segmentkey] as supersegment, 
 	st_union(links.segmentgeo::geometry, motors.segmentgeo::geometry) as geo,
 	'exit'::text as direction
-from experiments.rmp10_aalborg_segments links
-join experiments.rmp10_aalborg_segments motors
+from maps.osm_dk_20140101 links
+join maps.osm_dk_20140101 motors
 on
 	links.category='motorway_link' and 
 	motors.category='motorway' and
 	links.startpoint=motors.endpoint);
 
-insert into experiments.rmp10_supersegments_motorway_links (supersegment, geo, direction)
+insert into experiments.rmp10_supersegments_motorway_links (link_segment, supersegment, geo, direction)
 select 
+ 	links.segmentkey as link_segment,
 	array[links.segmentkey, motors.segmentkey] as supersegment, 
 	st_union(links.segmentgeo::geometry, motors.segmentgeo::geometry) as geo,
 	'entry'::text as direction
-from experiments.rmp10_aalborg_segments links
-join experiments.rmp10_aalborg_segments motors
+from maps.osm_dk_20140101 links
+join maps.osm_dk_20140101 motors
 on
 	links.category='motorway_link' and 
 	motors.category='motorway' and
 	links.endpoint=motors.startpoint
+
+
 
 
