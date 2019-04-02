715bb08808e3d85c979cb2ae3ea1379955a8472f
myrionsc <marpht23@gmail.com>
2019-04-02 09:28:18 +0200

    find trafic light nodes in ENTIRE country

--- SQL/nodes_within_10m_from_traffic_light.sql
+++ SQL/nodes_within_10m_from_traffic_light.sql
@@ -1,7 +1,7 @@
 select *
 into experiments.rmp10_trafic_light_nodes_within_10m
 from experiments.rmp10_traffic_lights tl
-join experiments.rmp10_interesting_nodes_aalborg n
+join experiments.rmp10_interesting_nodes n
 on st_dwithin(
 	ST_TRANSFORM(tl.way::geometry, 3857), 
 	ST_TRANSFORM(n.geom::geometry, 3857),
