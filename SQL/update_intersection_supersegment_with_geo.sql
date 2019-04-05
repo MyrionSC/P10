372dfd0a657ec885dec98c22e05cdb5b6ce0039e
myrionsc <marpht23@gmail.com>
2019-04-05 09:36:18 +0200

    update correct table grrrr

--- SQL/update_intersection_supersegment_with_geo.sql
+++ SQL/update_intersection_supersegment_with_geo.sql
@@ -1,4 +1,9 @@
-update experiments.rmp10_intersection_supersegments as iss
+ALTER TABLE experiments.rmp10_intersection_supersegments_v2
+ADD COLUMN start_geo geometry,
+ADD COLUMN end_geo geometry,
+ADD COLUMN supersegment_geo geometry;
+
+update experiments.rmp10_intersection_supersegments_v2 as iss
 SET 
 	start_geo=sq.start_geo,
 	end_geo=sq.end_geo,
@@ -11,14 +16,14 @@ FROM (
 			st_union(startseg.segmentgeo::geometry) as start_geo, 
 			st_union(endseg.segmentgeo::geometry) as end_geo,
 			st_union(mss.segmentgeo::geometry) as supersegment_geo
-		FROM experiments.rmp10_intersection_supersegments iiss
+		FROM experiments.rmp10_intersection_supersegments_v2 iiss
 		JOIN maps.osm_dk_20140101 startseg
 		ON iiss.in_segkey=startseg.segmentkey
 		JOIN maps.osm_dk_20140101 endseg
 		ON iiss.out_segkey=endseg.segmentkey
 		JOIN (
 			select cid, in_segkey, out_segkey, unnest(supersegment) as supersegs
-			FROM experiments.rmp10_intersection_supersegments
+			FROM experiments.rmp10_intersection_supersegments_v2
 		) supersegs
 		on iiss.cid=supersegs.cid and iiss.in_segkey=supersegs.in_segkey and iiss.out_segkey=supersegs.out_segkey
 		JOIN maps.osm_dk_20140101 mss
@@ -27,3 +32,4 @@ FROM (
 	) as sq
 WHERE
 	iss.cid=sq.cid and iss.in_segkey=sq.in_segkey and iss.out_segkey=sq.out_segkey;
+
