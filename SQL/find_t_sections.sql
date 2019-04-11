27646e31b1354e4955482e14f062d13622a5f817
myrionsc <marpht23@gmail.com>
2019-04-11 09:33:41 +0200

    find t sections query

---
+++ SQL/find_t_sections.sql
@@ -0,0 +1,15 @@
+select *
+from experiments.rmp10_interesting_nodes n
+into experiments.rmp10_t_sections
+where 
+	not EXISTS (
+		select 
+		from experiments.rmp10_intersections_v2 i
+		where n.point=i.point
+	) and
+	not EXISTS(
+		select
+		from experiments.rmp10_roundabouts r
+		where r.internal and (n.point=r.endpoint or n.point=r.startpoint)
+	)
+
