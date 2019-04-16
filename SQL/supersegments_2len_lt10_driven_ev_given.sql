b257cc629f95405757827f619f03742e91db6da8
myrionsc <marpht23@gmail.com>
2019-04-16 23:02:04 +0200

    supersegments_2len_lt10_driven_ev_given.sql

--- SQL/supersegments_2len_lt10_driven_ev_given.sql
+++ SQL/supersegments_2len_lt10_driven_ev_given.sql
@@ -1,11 +1,11 @@
 SELECT seg, given, ev.avg_ev as single_ev, ev_given.avg_ev as given_ev
-INTO experiments.rmp10_supersegments_2len_mt10_driven_ev_given
+INTO experiments.rmp10_supersegments_2len_lt10_driven_ev_given
 FROM ev((
     SELECT array_agg(segments[2])
     FROM experiments.rmp10_driven_supersegments_2len
-    WHERE num_traversals >= 10
+    WHERE num_traversals < 10
 )) JOIN ev_given ((
     SELECT array_agg(ARRAY[segments[2], segments[1]])
     FROM experiments.rmp10_driven_supersegments_2len
-    WHERE num_traversals >= 10
+    WHERE num_traversals < 10
 )) ON ev.segmentkey = ev_given.seg
