7496a6fa46c7d0c10022918bd1e213ef61ac9759
myrionsc <marpht23@gmail.com>
2019-04-16 14:55:38 +0200

    more sql

--- SQL/driven_supersegments_2len_mt10_driven.sql
+++ SQL/driven_supersegments_2len_mt10_driven.sql
@@ -10,6 +10,7 @@ SELECT ARRAY[osm1.segmentkey, osm2.segmentkey] as segments
 FROM experiments.rmp10_osm_dk_20140101_driven osm1
 JOIN experiments.rmp10_osm_dk_20140101_driven osm2
 ON
+	osm1.segmentkey!=osm2.segmentkey AND	
 	osm1.num_traversals>=10 AND
 	osm2.num_traversals>=10 AND
 	(osm1.endpoint = ANY(ARRAY[osm2.startpoint, osm2.endpoint]) OR
