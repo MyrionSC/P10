1ee0ead88839858b16e7552273216f917658b559
myrionsc <marpht23@gmail.com>
2019-05-08 11:04:27 +0200

    trips_aggregated.sql

---
+++ SQL/trips_aggregated.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS experiments.rmp10_trips_aggregated;
+
+select 
+	trip_id,
+	max(trip_segmentno) as trip_segments_count,
+	array_agg(segmentkey) as segmentkeys_arr,
+	array_agg(meters_driven) as meters_driven_arr,
+	array_agg(seconds) as seconds_arr,
+	array_agg(ev_kwh) as ev_kwh_arr,
+	array_agg(datekey) as datekey_arr,
+	array_agg(timekey) as timekey_arr
+into experiments.rmp10_trips_aggregated
+from mapmatched_data.viterbi_match_osm_dk_20140101
+group by trip_id;
+
+CREATE INDEX rmp10_trips_aggregated_trip_idx
+    ON experiments.rmp10_trips_aggregated USING btree
+    (trip_id ASC NULLS LAST)
+    TABLESPACE pg_default;
