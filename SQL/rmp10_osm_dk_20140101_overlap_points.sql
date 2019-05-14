0de9f8131bbdf312f0f721fbaedddf8d6e97a12e
Rasmus Møller Jensen <jensen.rasmusmoller@gmail.com>
2019-05-14 09:52:27 +0200

    Forgot to save on of the files xD

--- SQL/rmp10_osm_dk_20140101_overlap_points.sql
+++ SQL/rmp10_osm_dk_20140101_overlap_points.sql
@@ -36,3 +36,16 @@ WHERE op.point = ss.point;
 UPDATE experiments.rmp10_osm_dk_20140101_overlaps_points op
 SET traffic_lights = False
 WHERE traffic_lights is null;
+
+ALTER TABLE experiments.rmp10_osm_dk_20140101_overlaps_points
+ADD COLUMN height double precision;
+
+UPDATE experiments.rmp10_osm_dk_20140101_overlaps_points op
+SET height = h
+FROM (
+    SELECT p.point, ST_NearestValue(r.rast, p.geo::geometry) as h
+    FROM experiments.bcj_heightmapbridge r
+    JOIN experiments.rmp10_osm_dk_20140101_overlaps_points p
+    ON ST_Intersects(p.geo::geometry, r.rast)
+) sub
+WHERE sub.point = op.point;
