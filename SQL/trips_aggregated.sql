f696e729183d9bd748d7aa0387cc8ad3fa147114
myrionsc <marpht23@gmail.com>
2019-05-15 15:39:15 +0200

    renamed trips_aggregated_new to trips_aggregated and changed it in db as well

--- SQL/trips_aggregated.sql
+++ SQL/trips_aggregated.sql
@@ -14,14 +14,18 @@ select
 	array_agg(timekey) as timekey_arr
 into experiments.rmp10_trips_aggregated
 from (
-	select *
-	from mapmatched_data.viterbi_match_osm_dk_20140101
-	order by trip_segmentno
-) sq
+	SELECT * 
+	FROM experiments.rmp10_viterbi_match_osm_dk_20140101_overlap_new 
+	ORDER BY trip_id, trip_segmentno
+) sub
 group by trip_id;
 
-CREATE INDEX rmp10_trips_aggregated_trip_idx
+CREATE INDEX rmp10_trips_aggregated_trip_id_idx
     ON experiments.rmp10_trips_aggregated USING btree
     (trip_id ASC NULLS LAST)
+    TABLESPACE pg_default;
+CREATE INDEX rmp10_trips_aggregated_segmentkeys_arr_idx 
+    ON experiments.rmp10_trips_aggregated USING gin
+    (segmentkeys_arr)
     TABLESPACE pg_default;
 
