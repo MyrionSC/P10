1259e2f6946009018db7bd8a2832f61df9630813
myrionsc <marpht23@gmail.com>
2019-05-17 16:43:02 +0200

    added height difference and incline from bcj_incline to supersegments original tables

--- SQL/all_trip_supersegments_original.sql
+++ SQL/all_trip_supersegments_original.sql
@@ -24,6 +24,8 @@ select
 	(SELECT SUM(s) FROM UNNEST(ta.meters_segment_arr[sst.start_segmentno:sst.end_segmentno]) s) AS meters_segment,
 	(SELECT SUM(s) FROM UNNEST(ta.meters_driven_arr[sst.start_segmentno:sst.end_segmentno]) s) AS meters_driven,
 	(SELECT SUM(s) FROM UNNEST(ta.seconds_arr[sst.start_segmentno:sst.end_segmentno]) s) AS seconds,
+	(SELECT SUM(s) FROM UNNEST(ta.incline_arr[sst.start_segmentno:sst.end_segmentno]) s) AS incline,
+	(SELECT SUM(s) FROM UNNEST(ta.height_difference_arr[sst.start_segmentno:sst.end_segmentno]) s) AS height_difference,
 	(SELECT SUM(s) FROM UNNEST(ta.ev_kwh_arr[sst.start_segmentno:sst.end_segmentno]) s) * 1000 AS ev_wh,
 	(SELECT MIN(s) FROM UNNEST(ta.datekey_arr[sst.start_segmentno:sst.end_segmentno]) s) AS datekey,
 	(SELECT MIN(s) FROM UNNEST(ta.timekey_arr[sst.start_segmentno:sst.end_segmentno]) s) AS timekey,
--- SQL/trips_aggregated_original.sql
+++ SQL/trips_aggregated_original.sql
@@ -7,23 +7,34 @@ select
 	array_agg(meters_segment) as meters_segment_arr,
 	array_agg(meters_driven) as meters_driven_arr,
 	array_agg(speed) as speed_arr,
+	array_agg(height_difference) as height_difference_arr,
+	array_agg(incline) as incline_arr,
 	array_agg(weathermeasurekey) as weathermeasurekey_arr,
 	array_agg(seconds) as seconds_arr,
 	array_agg(updated_ev_kwh) as ev_kwh_arr,
 	array_agg(datekey) as datekey_arr,
 	array_agg(timekey) as timekey_arr,
 	array_agg(id) as id_arr
-into experiments.rmp10_trips_aggregated_original
 from (
 	select 
 		v.*,
 		CASE 
-			WHEN e.ev_kwh IS null THEN v.ev_kwh
-			ELSE e.ev_kwh
-		END AS updated_ev_kwh
+			WHEN e.ev_kwh_from_ev_watt IS null THEN v.ev_kwh
+			ELSE e.ev_kwh_from_ev_watt
+		END AS updated_ev_kwh,
+		CASE
+			WHEN v.direction='FORWARD' THEN i.height_difference
+			ELSE i.height_difference * -1
+		END AS height_difference,
+		CASE
+			WHEN v.direction='FORWARD' THEN i.incline
+			ELSE i.incline * -1
+		END AS incline
 	from mapmatched_data.viterbi_match_osm_dk_20140101 v
 	left outer join experiments.bcj_ev_watt_data e
 	on v.id=e.id
+	join experiments.bcj_incline i
+	on v.segmentkey=i.segmentkey
 	order by trip_id, trip_segmentno
 ) sq
 group by trip_id;
