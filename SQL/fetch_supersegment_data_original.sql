886af6277fbe1f6aa9b42d6685301a00971be16c
myrionsc <marpht23@gmail.com>
2019-05-17 13:19:20 +0200

    fetch_supersegment_data.sql updated and fetch_supersegment_data_original.sql added

--- SQL/fetch_supersegment_data_original.sql
+++ SQL/fetch_supersegment_data_original.sql
@@ -1,5 +1,5 @@
-DROP VIEW IF EXISTS experiments.supersegments_temp_view;
-CREATE VIEW experiments.supersegments_temp_view AS
+DROP VIEW IF EXISTS experiments.supersegments_temp_view_original;
+CREATE VIEW experiments.supersegments_temp_view_original AS
 	SELECT
 		ats.trip_id,
 		sups.segments, -- maybe not this
@@ -17,9 +17,9 @@ CREATE VIEW experiments.supersegments_temp_view AS
 		ats.ev_wh,
 		ats.datekey,
 		ats.timekey
-	FROM experiments.rmp10_all_supersegments sups
-	JOIN experiments.rmp10_all_trip_supersegments ats -- only take those that overlaps with rmp10_training
-	ON sups.segments=ats.segments AND sups.type=ats.type
+	FROM experiments.rmp10_all_supersegments_original sups
+	JOIN experiments.rmp10_all_trip_supersegments_original ats -- only take those that overlaps with rmp10_training
+	ON sups.superseg_id=ats.superseg_id
 	UNION
 	SELECT
 		t.trip_id,
@@ -38,8 +38,7 @@ CREATE VIEW experiments.supersegments_temp_view AS
 		CASE 
 			WHEN e.ev_kwh_from_ev_watt IS null THEN t.ev_kwh
 			ELSE e.ev_kwh_from_ev_watt
-		END
-		t.ev_kwh * 1000 as ev_wh,
+		END AS ev_wh,
 		t.datekey,
 		t.timekey
 	FROM experiments.rmp10_training t
@@ -50,4 +49,5 @@ CREATE VIEW experiments.supersegments_temp_view AS
 	JOIN experiments.bcj_incline inc
 	ON t.segmentkey=inc.segmentkey;
 
-\copy (SELECT * FROM experiments.supersegments_temp_view limit 100) TO '../Models/data/supersegment-data.csv' HEADER CSV;  
+
+\copy (SELECT * FROM experiments.supersegments_temp_view_original limit 100) TO '../Models/data/supersegment-data-original.csv' HEADER CSV;  
