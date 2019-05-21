02499b4dfd07697712baae63def4e83421b1abb7
myrionsc <marpht23@gmail.com>
2019-05-21 16:31:27 +0200

    fetch_supersegment_data.sql join with new viterbi

--- SQL/fetch_supersegment_data.sql
+++ SQL/fetch_supersegment_data.sql
@@ -1,13 +1,16 @@
 DROP VIEW IF EXISTS experiments.supersegments_training_data_temp_view;
 CREATE VIEW experiments.supersegments_training_data_temp_view AS
+SELECT *
+FROM experiments.rmp10_all_trip_data atd
+WHERE EXISTS (
 	SELECT
-		*
-	FROM experiments.rmp10_all_trip_data atd
-	WHERE EXISTS (
-		SELECT
+	FROM (
+		SELECT v.id
 		FROM experiments.rmp10_training t
-		WHERE t.id=any(atd.id_arr)
-	);
-
+		JOIN experiments.rmp10_viterbi_match_osm_dk_20140101_overlap v
+		ON t.id=v.origin
+	) sq
+	WHERE sq.id=any(atd.id_arr)
+);
 \copy (SELECT * FROM experiments.supersegments_training_data_temp_view) TO '../Models/data/supersegment-data.csv' HEADER CSV;
 
