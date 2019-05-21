031657bc09029f8c7e9c7b25667e92d40e64e160
myrionsc <marpht23@gmail.com>
2019-05-21 15:51:21 +0200

    fetch_supersegment_data.sql updated

--- SQL/fetch_supersegment_data.sql
+++ SQL/fetch_supersegment_data.sql
@@ -1,53 +1,13 @@
-DROP VIEW IF EXISTS experiments.supersegments_temp_view;
-CREATE VIEW experiments.supersegments_temp_view AS
+DROP VIEW IF EXISTS experiments.supersegments_training_data_temp_view;
+CREATE VIEW experiments.supersegments_training_data_temp_view AS
 	SELECT
-		ats.trip_id,
-		sups.segments, -- maybe not this
-		sups.type, -- maybe not this
-		sups.direction,
-		sups.traffic_lights,
-		sups.height_difference,
-		sups.categories[1] as cat_start,
-		sups.categories[array_length(sups.categories, 1)] as cat_end,
-		sups.cat_speed_difference,
-		ats.meters_driven,
-		ats.meters_segment,
-		ats.weathermeasurekey,
-		ats.seconds,
-		ats.ev_wh,
-		ats.datekey,
-		ats.timekey
-	FROM experiments.rmp10_all_supersegments sups
-	JOIN experiments.rmp10_all_trip_supersegments ats -- only take those that overlaps with rmp10_training
-	ON sups.segments=ats.segments AND sups.type=ats.type
-	UNION
-	SELECT
-		t.trip_id,
-		array[t.segmentkey] as segments,
-		'Segment' as type,
-		'Straight' as direction,
-		'False' as traffic_lights,
-		inc.height_difference,
-		s.category as cat_start,
-		s.category as cat_end,
-		0 as cat_speed_difference,
-		t.meters_driven,
-		t.meters_segment,
-		t.weathermeasurekey,
-		t.seconds,
-		CASE 
-			WHEN e.ev_kwh_from_ev_watt IS null THEN t.ev_kwh
-			ELSE e.ev_kwh_from_ev_watt
-		END
-		t.ev_kwh * 1000 as ev_wh,
-		t.datekey,
-		t.timekey
-	FROM experiments.rmp10_training t
-	JOIN maps.osm_dk_20140101 s 
-	ON t.segmentkey=s.segmentkey
-	LEFT OUTER JOIN experiments.bcj_ev_watt_data e
-	ON t.id=e.id
-	JOIN experiments.bcj_incline inc
-	ON t.segmentkey=inc.segmentkey;
+		*
+	FROM experiments.rmp10_all_trip_data atd
+	WHERE EXISTS (
+		SELECT
+		FROM experiments.rmp10_training t
+		WHERE t.id=any(atd.id_arr)
+	);
 
-\copy (SELECT * FROM experiments.supersegments_temp_view limit 100) TO '../Models/data/supersegment-data.csv' HEADER CSV;  
+\copy (SELECT * FROM experiments.supersegments_training_data_temp_view) TO '../Models/data/supersegment-data.csv' HEADER CSV;
+
