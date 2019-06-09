2770b53e85c344a2902657decf02b03a675ba2f8
Rasmus Møller Jensen <jensen.rasmusmoller@gmail.com>
2019-06-09 10:57:53 +0200

    Predict new stuff

---
+++ scripts/predict_all_features.py
@@ -0,0 +1,8 @@
+from DNNRegressor import predict
+from batch_configs.supersegment_features_grid import configs
+
+for conf in configs:
+	first = True
+	if "Iter_1" in conf['model_name_base']
+		predict(conf, True, first)
+		firts = False
